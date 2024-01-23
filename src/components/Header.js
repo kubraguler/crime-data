a26f7ea56ae1f423489f14788c804b92df40f206
kubraguler <oktnkbr@gmail.com>
2024-01-23 23:59:36 +0000

    Implement Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<header className="max-w-xl mx-auto my-20">
+			<h1 className="text-4xl font-bold text-primary my-5">Crime Data</h1>
+			<h2 className="text-xl text-primary">search by postcode</h2>
+		</header>
+	);
+};
+
+export default Header;
