c00a95538b7a522139de9b0b6fcaeb41b9ebd53b
kubraguler <oktnkbr@gmail.com>
2024-01-25 12:49:00 +0000

    Display crime data on the screen

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,6 +4,7 @@ import { formatCategoryTitle } from "../utils/utils";
 const Accordion = ({ crimeData }) => {
 	const [categories, setCategories] = useState([]);
 	const [activeIndex, setActiveIndex] = useState(null);
+	const [groupedCrimeData, setGroupedCrimeData] = useState({});
 
 	const toggleAccordion = (index) => {
 		setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
@@ -13,38 +14,54 @@ const Accordion = ({ crimeData }) => {
 		const crimeCategoryList = crimeData.map((item) => item.category);
 		const uniqueCategories = Array.from(new Set(crimeCategoryList));
 		setCategories(uniqueCategories);
-	}, [crimeData]);
 
-	console.log(crimeData);
+		const groupedData = uniqueCategories.reduce((acc, category) => {
+			acc[category] = crimeData.filter((item) => item.category === category);
+			return acc;
+		}, {});
+		setGroupedCrimeData(groupedData);
+	}, [crimeData]);
 
 	return (
 		<div>
 			{categories.map((item, index) => (
 				<div key={index} className="mb-4">
 					<div className="bg-gray-200 p-4 cursor-pointer" onClick={() => toggleAccordion(index)}>
-						<h2 className="text-lg text-left font-normal">{formatCategoryTitle(item)}</h2>
+						<h2 className="flex items-center justify-between text-lg text-left font-normal relative">
+							{formatCategoryTitle(item)}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								className={`w-5 h-5 inline-block  transition-all ease-in-out duration-300 ${activeIndex === index ? "rotate-180" : "rotate-0"}`}
+							>
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+							</svg>
+						</h2>
 					</div>
 					{activeIndex === index && (
-						<>
-							<div className="bg-white p-4 text-left border-b-4 border-sky-950-">
-								<p className="leading-loose">Postcode: </p>
-								<hr></hr>
-								<p className="leading-loose">Date of Crime: </p>
-								<hr></hr>
-								<p className="leading-loose">Approximate Street Name: </p>
-								<hr></hr>
-								<p className="leading-loose">Outcome Status: </p>
-							</div>
-							<div className="bg-white p-4 text-left">
-								<p className="leading-loose">Postcode: </p>
-								<hr></hr>
-								<p className="leading-loose">Date of Crime: </p>
-								<hr></hr>
-								<p className="leading-loose">Approximate Street Name: </p>
-								<hr></hr>
-								<p className="leading-loose">Outcome Status: </p>
-							</div>
-						</>
+						<div className="bg-white p-4 text-left">
+							{groupedCrimeData[item].map((crime, crimeIndex) => (
+								<div key={crimeIndex} className="border-b-4">
+									<p className="leading-loose">
+										<b>Postcode:</b> {crime.postcode}
+									</p>
+									<hr></hr>
+									<p className="leading-loose">
+										<b>Date of Crime:</b> {crime.month}
+									</p>
+									<hr></hr>
+									<p className="leading-loose">
+										<b>Approximate Street Name:</b> {crime.location.street.name}
+									</p>
+									<hr></hr>
+									<p className="leading-loose">
+										<b>Outcome Status:</b> {crime.outcome_status?.category ? crime.outcome_status.category : "On Going"}
+									</p>
+								</div>
+							))}
+						</div>
 					)}
 				</div>
 			))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {}
+		extend: {
+			rotate: {
+				180: "180deg"
+			}
+		}
 	},
 	plugins: [require("@tailwindcss/line-clamp")]
 };
